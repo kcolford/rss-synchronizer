2e0996ed92476f9f7116b59ea5d2db022ab042e0
Kieran Colford <kieran@kcolford.com>
2016-09-14 09:03:24 -0400

    cleaned up whitespace and prepared more additions

--- rss-synchronizer.js
+++ rss-synchronizer.js
@@ -20,39 +20,55 @@ function xml(body) {
   }
 }
 
+function caching_request() {
+  var request_cache = {};
+  var old_request = request;
+  return function(url, opts, cb) {
+    if (url in request_cache) {
+      cb.apply(null, request_cache[url]);
+    }
+    old_request(url, opts, function() {
+      request_cache[url] = Array.from(arguments);
+      cb.apply(null, arguments);
+    });
+  }
+}
+
 function aggregate() {
+
   db.serialize(function() {
     db.run(
       'create table if not exists sendto (email, url, category, last_update)'
     );
 
+    //var request = caching_request();
     db.each(
       'select *, rowid from sendto',
       function(err, row) {
-        if (err)
-          return console.error('failed to fetch data from database');
+	if (err)
+	  return console.error('failed to fetch data from database');
 
-        if (!row.last_update) {
+	if (!row.last_update) {
 
-          // initialize the date field
-          db.run(
-            'update sendto set last_update = ? where rowid = ?',
-            [Date.now(), row.rowid]
-          );
+	  // initialize the date field
+	  db.run(
+	    'update sendto set last_update = ? where rowid = ?',
+	    [Date.now(), row.rowid]
+	  );
 
-        } else {
+	} else {
 
-          request(row.url, {gzip: true}, function(err, response, body) {
-            if (err || response.statusCode != 200)
-              return console.error('failed to fetch url', row.url);
+	  request(row.url, {gzip: true}, function(err, response, body) {
+	    if (err || response.statusCode != 200)
+	      return console.error('failed to fetch url', row.url);
 
-            var data = xml(body);
-            if (!data)
-              return console.error('failed to parse xml from', row.url);
+	    var data = xml(body);
+	    if (!data)
+	      return console.error('failed to parse xml from', row.url);
 
-            var channel = data.rss[0].channel[0];
-            var items = channel.item || [];
-            for (var i = 0; i < items.length; i++) {
+	    var channel = data.rss[0].channel[0];
+	    var items = channel.item || [];
+	    for (var i = 0; i < items.length; i++) {
 
 	      if (!(true &&
 		    items[i] &&
@@ -69,45 +85,46 @@ function aggregate() {
 		continue;
 	      }
 
-              // filter by category if it is given
-              if (row.category &&
+	      // filter by category if it is given
+	      if (row.category &&
 		  items[i].category &&
 		  items[i].category.indexOf(row.category) == -1)
-                continue;
+		continue;
 
-              var pubDate = Date.parse(items[i].pubDate[0]);
+	      var pubDate = Date.parse(items[i].pubDate[0]);
 
-              // don't fetch something that's old
-              if (pubDate <= row.last_update)
-                continue;
+	      // don't fetch something that's old
+	      if (pubDate <= row.last_update)
+		continue;
 
-              emailer.sendMail({
-                from: process.env.FROM || 'RSS <rss-noreply@kcolford.com>',
-                to: row.email,
-                subject: items[i].title[0],
-                html: items[i].description[0] + '<br/><a href="' + items[i].link[0] + '">click here</a>'
-              }, function(err, info) {
-                if (err)
-                  return console.error(err);
-                console.log('sent email', info);
+	      emailer.sendMail({
+		from: process.env.FROM || 'RSS <rss-noreply@kcolford.com>',
+		to: row.email,
+		subject: items[i].title[0],
+		html: items[i].description[0] + '<br/><a href="' + items[i].link[0] + '">click here</a>'
+	      }, function(err, info) {
+		if (err)
+		  return console.error(err);
+		console.log('sent email', info);
 
-                // record the last updated entry, but
-                db.run(
-                  'update sendto set last_update = max(?, last_update) where rowid = ?',
-                  [Date.now(), row.rowid],
-                  function() {
-                    console.log('updated record', arguments);
-                  }
-                );
+		// record the last updated entry, but
+		db.run(
+		  'update sendto set last_update = max(?, last_update) where rowid = ?',
+		  [Date.now(), row.rowid],
+		  function() {
+		    console.log('updated record', arguments);
+		  }
+		);
 
-              });
-            }
-          });
+	      });
+	    }
+	  });
 
-        }
+	}
       }
     );
   });
 }
 
+//setInterval(aggregate, process.env.RSS_INTERVAL);
 aggregate();
