379234f0799dc92a5998729877067a1e61ba90f6
Kieran Colford <kieran@kcolford.com>
2016-10-06 09:21:49 -0400

    fixed nodejs dependency

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 cd "$(dirname "$0")"
 
-pacman -S --asdeps --needed node npm phantomjs systemd
+pacman -S --asdeps --needed nodejs npm phantomjs systemd
 npm install -g
 install -m644 rss-synchronizer.service /etc/systemd/system/
 systemctl daemon-reload
