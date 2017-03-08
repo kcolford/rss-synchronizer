09c175b9594901f6c98bfa9c998ad65dbba98148
Kieran Colford <kieran@kcolford.com>
2017-03-08 11:55:04 -0500

    some minor changes

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-set -eo pipefail
-cd "$(dirname "$0")"
-
+cd "$(dirname "$0")" || exit
 pacman -S --asdeps --needed nodejs npm phantomjs systemd
 npm install -g
-install -m644 rss-synchronizer.service /etc/systemd/system/
+cp rss-synchronizer.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl restart rss-synchronizer
